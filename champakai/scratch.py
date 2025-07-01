c7981acab89eab4850e0590d5e9fbec97be1bf9d
champaksworldcreate <champaksworld@gmail.com>
2025-07-01 09:07:12 +0530

    Tips for July 2025 added

---
+++ champakai/scratch.py
@@ -0,0 +1,14 @@
+# n = 8765
+# count = 0
+# if n == 0:
+#     print(f"No of digits={1}")
+# else:
+
+#     while n > 0:
+#         n = n//10
+#         count += 1
+#     print(f"No of digits={count}")
+src="Tokyo"
+dest = "London"
+# print("This is a train from Nai Basti to London ")
+print(f"This is a train from {src} to {dest} ")
